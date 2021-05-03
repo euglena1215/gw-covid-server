c8578a862386c2cf367df80b4ae72d907477e042
Teppei Shintani <teppest1215@gmail.com>
2021-05-03 19:34:12 +0900

    POST /room/join

--- main.go
+++ main.go
@@ -30,19 +30,57 @@ func handleCreateRoom(c echo.Context) error {
 	roomId, _ := uuid.NewUUID()
 	roomStmt, roomErr := db.Prepare("INSERT INTO rooms(id) VALUES($1)")
 	if roomErr != nil {
-		log.Fatal(roomErr)
+		return roomErr
 	}
 	roomStmt.Exec(roomId.String())
 
 	userId, _ := uuid.NewUUID()
 	userStmt, userErr := db.Prepare("INSERT INTO users(id, room_id) VALUES($1,$2)")
 	if userErr != nil {
-		log.Fatal(userErr)
+		return userErr
 	}
 	userStmt.Exec(userId.String(), roomId.String())
 
 	res := CreateRoomResponse{
 		RoomId: roomId.String(),
+		UserId: userId.String(),
+	}
+
+	return c.JSON(http.StatusOK, res)
+}
+
+type JoinRoomRequest struct {
+	RoomId string `json:"room_id"`
+}
+
+type JoinRoomResponse struct {
+	UserId string `json:"user_id"`
+}
+
+func handleJoinRoom(c echo.Context) error {
+	req := new(JoinRoomRequest)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+
+	db := connectDb()
+	defer db.Close()
+
+	var exists int
+	err := db.QueryRow("SELECT 1 FROM rooms WHERE id = $1", req.RoomId).Scan(&exists)
+	if err != nil {
+		// 対応するレコードが見つからなかった場合もここにくる
+		return c.String(http.StatusBadRequest, "{}")
+	}
+
+	userId, _ := uuid.NewUUID()
+	userStmt, userErr := db.Prepare("INSERT INTO users(id, room_id) VALUES($1,$2)")
+	if userErr != nil {
+		return userErr
+	}
+	userStmt.Exec(userId.String(), req.RoomId)
+	
+	res := JoinRoomResponse{
 		UserId: userId.String(),
 	}
 
@@ -90,6 +128,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.GET("/ping", handlePing)
 	e.POST("/room", handleCreateRoom)
+	e.POST("/room/join", handleJoinRoom)
 	e.GET("/ws", handleWebSocket)
 	e.Static("/", "public")
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
